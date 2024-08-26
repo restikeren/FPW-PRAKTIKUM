d151497cb1defabc1afa4531513d033f747240dd
resti <160569456+restikeren@users.noreply.github.com>
2024-08-26 15:22:29 +0700

    Update halo.py

--- halo.py
+++ halo.py
@@ -1,11 +1,7 @@
-# Kode Python untuk output yang indah
-
-# Definisikan variabel
 nama = "Resti Dwi Artika"
 npm = "2210631170042"
 mata_kuliah = "Framework Programming Web"
 
-# Format output
 output = f"""
 {'*' * 50}
               Halo, 
